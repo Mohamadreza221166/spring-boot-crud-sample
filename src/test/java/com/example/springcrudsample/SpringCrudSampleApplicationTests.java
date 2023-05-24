5d40e0c8b7938aa6661ddf5fc50bee8069bee4ef
MohammadReza221166 <mohamadreza221166@gmail.com>
2023-05-24 09:28:00 +0430

    first push

---
+++ src/test/java/com/example/springcrudsample/SpringCrudSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcrudsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
